63d19b5dc42048cbda78f8522706c8d60685b85b
Akhilesh Prathuri <Akhilesh.Prathuri@jda.com>
2017-10-26 10:05:57 +0530

    Add project files.

---
+++ HackerRank/HourGlass.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+  public class HourGlass
+  {
+    public static void Main(string[] args)
+    {
+      int[][] arr = new int[6][];
+      for (int arr_i = 0; arr_i < 6; arr_i++)
+      {
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+      }
+
+      Console.WriteLine(CalMaxHourGlassValue(arr));
+
+    }
+
+    public static int CalMaxHourGlassValue(int[][] arr)
+    {
+      var hourGlasses = SplitIntoHourGlasses(arr);
+      var maxValue = GetMaxHourGlassValue(hourGlasses);
+      return maxValue;
+    }
+
+    private static int GetMaxHourGlassValue(List<int[][]> hourGlasses)
+    {
+      int maxHourGlassSum = -100;
+      for (int i = 0; i < hourGlasses.Count; i++)
+      {
+        var hourGlass = hourGlasses[i];
+        var sum = hourGlass[0][0] + hourGlass[0][1] + hourGlass[0][2]
+                                  + hourGlass[1][1]
+                 + hourGlass[2][0] + hourGlass[2][1] + hourGlass[2][2];
+        if (sum > maxHourGlassSum)
+        {
+          maxHourGlassSum = sum;
+        }
+      }
+        return maxHourGlassSum;
+    }
+
+    private static List<int[][]> SplitIntoHourGlasses(int[][] input)
+    {
+      var columnIteraton = 4;
+      var rowIteration = 4;
+      List<int[][]> hourGlasses = new List<int[][]>();
+      for (int j = 0; j < columnIteraton; j++)
+      {
+        for (int i = 0; i < rowIteration; i++)
+        {
+          int[][] hourGlass = new int[3][];
+          hourGlass[0] = new int[] { input[i][j], input[i][j + 1], input[i][j + 2] };
+          hourGlass[1] = new int[] { input[i + 1][j], input[i + 1][j + 1], input[i + 1][j + 2] };
+          hourGlass[2] = new int[] { input[i + 2][j], input[i + 2][j + 1], input[i + 2][j + 2] };
+          hourGlasses.Add(hourGlass);
+        }
+
+      }
+      return hourGlasses;
+    }
+  }
+}
---
+++ HackerRankTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank;
+
+namespace HackerRankTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestHourGlass()
+    {
+      int[][] input = new int[6][];
+
+      input[0] = new int[] { 1, 1, 1, 0, 0, 0 };
+      input[1] = new int[] { 0, 1, 0, 0, 0, 0 };
+      input[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+      input[3] = new int[] { 0, 0, 2, 4, 4, 0 };
+      input[4] = new int[] { 0, 0, 0, 2, 0, 0 };
+      input[5] = new int[] { 0, 0, 1, 2, 4, 0 };
+
+      int[][] input2 = new int[6][];
+
+      input2[0] = new int[] { 0 ,- 4 ,- 6, 0, - 7, - 6 };
+      input2[1] = new int[] { -1 ,- 2 ,- 6, - 8, - 3 ,- 1 };
+      input2[2] = new int[] { -8, - 4, - 2, - 8 ,- 8, - 6 };
+      input2[3] = new int[] { -3 ,- 1 ,- 2, - 5, - 7, - 4 };
+      input2[4] = new int[] { -3, - 5, - 3, - 6 ,- 6, - 6 };
+      input2[5] = new int[] { -3 ,- 6, 0 ,- 8, - 6, - 7 };
+
+
+      var maxHourGlass = HourGlass.CalMaxHourGlassValue(input2);
+
+      Assert.AreEqual(-19, maxHourGlass);
+    }
+  }
+}
